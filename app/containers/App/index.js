c3d10e679b851100874ab772e969ba7afcbec3d4
quan <anhquannguyen303@gmail.com>
2022-05-30 16:41:47 +0700

    add login page

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -14,6 +14,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from 'containers/HomePage/Loadable';
 import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import Login from 'containers/Login/Loadable'
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 
@@ -40,7 +41,9 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/features" component={FeaturePage} />
+        <Route path="/login" component={Login} />
         <Route path="" component={NotFoundPage} />
+        
       </Switch>
       {/* <Footer /> */}
       <GlobalStyle />
