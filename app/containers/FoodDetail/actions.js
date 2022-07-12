02eeb8368d64704c1172e6dd50e26d1a91f8e4f2
quan <anhquannguyen303@gmail.com>
2022-07-12 14:31:25 +0700

    update package

--- app/containers/FoodDetail/actions.js
+++ app/containers/FoodDetail/actions.js
@@ -5,9 +5,34 @@
  */
 
 import { DEFAULT_ACTION } from './constants';
+import * as types from './constants';
 
 export function defaultAction() {
   return {
     type: DEFAULT_ACTION,
   };
 }
+
+export function getFoodById(payload) {
+  return {
+    type: types.GET_FOOD_BY_ID,
+    payload
+  };
+}
+
+
+export function getFoodByIdSuccess(payload) {
+  return {
+    type: types.GET_FOOD_BY_ID_SUCCESS,
+    payload
+  };
+}
+
+
+export function getFoodByIdFailed(payload) {
+  return {
+    type: types.GET_FOOD_BY_ID_FAILED,
+    payload
+  };
+}
+
--- app/containers/FoodDetail/saga.js
+++ app/containers/FoodDetail/saga.js
@@ -1,6 +1,17 @@
-// import { take, call, put, select } from 'redux-saga/effects';
+import { take, call, put, select, takeEvery } from 'redux-saga/effects';
+import { getFoodByIdFailed } from './actions';
+import { apiFetchData } from './api';
+import * as types from './constants';
 
+export function* getFoodById({ payload }) {
+  try {
+    const res = yield call(apiFetchData, []);
+  } catch (error) {
+    yield put(getFoodByIdFailed(error.message));
+  }
+}
 // Individual exports for testing
 export default function* foodDetailSaga() {
   // See example in containers/HomePage/saga.js
+  yield takeEvery(types.GET_FOOD_BY_ID, getFoodById)
 }
